47c8ffaa2875d3af46f4bd258a58885e15352b3b
shaunieboy31 <157598882+shaunieboy31@users.noreply.github.com>
2025-06-08 10:08:12 +0800

    DATABASE

--- database/binalots.sql
+++ database/binalots.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2025 at 04:38 AM
+-- Generation Time: Jun 08, 2025 at 04:07 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -20,6 +20,128 @@ SET time_zone = "+00:00";
 --
 -- Database: `binalots`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `receipt_no` varchar(32) NOT NULL,
+  `operator` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `receipt_no`, `operator`, `created_at`) VALUES
+(1, '20250605052303', 'Shaun', '2025-06-05 03:23:03'),
+(2, '20250605052627', 'Shaun', '2025-06-05 03:26:27'),
+(3, '20250605053440', 'Shaun', '2025-06-05 03:34:40'),
+(4, '20250605053510', 'Shaun', '2025-06-05 03:35:10'),
+(5, '20250607093306', 'Shaun', '2025-06-07 07:33:06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_details`
+--
+
+CREATE TABLE `order_details` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `item` varchar(255) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_details`
+--
+
+INSERT INTO `order_details` (`id`, `order_id`, `item`, `quantity`, `price`) VALUES
+(1, 1, 'Tapsilog', 1, 100.00),
+(2, 1, 'Tosilog', 1, 90.00),
+(3, 2, 'Tapsilog', 1, 100.00),
+(4, 2, 'Tosilog', 1, 90.00),
+(5, 3, 'Tosilog', 1, 90.00),
+(6, 4, 'Tapsilog', 1, 100.00),
+(7, 4, 'Tosilog', 1, 90.00),
+(8, 4, 'Longsilog', 1, 80.00),
+(9, 4, 'Bangsilog', 1, 110.00),
+(10, 5, 'Tapsilog', 1, 100.00),
+(11, 5, 'Tosilog', 2, 180.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`) VALUES
+(12, 'Test', 'test@gmail.com', '$2y$10$me9.hDgwp6xkURWR1brSpuMJneZ04rgTGImijJ4qpcNglPcaWEXOC', '2025-03-04 17:49:32'),
+(13, 'Shaun', 's@gmail.com', '$2y$10$w5Yt2aj/gN/nIViO2oie0.D3BjEY1h08vjlMz0OmBklf20cSapwFC', '2025-06-04 15:56:55');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `order_details`
+--
+ALTER TABLE `order_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
